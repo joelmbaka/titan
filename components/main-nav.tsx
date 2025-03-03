f1f6ccff964b74ac88206cfa3467fe9f3fdaa2e1
joelmbaka <dev@joelmbaka.site>
2025-03-03 16:06:59 +0300

    try blogpost modal

--- components/main-nav.tsx
+++ components/main-nav.tsx
@@ -104,26 +104,7 @@ export function MainNav() {
           </NavigationMenuContent>
         </NavigationMenuItem>
 
-        {/* Debug Tools */}
-        <NavigationMenuItem>
-          <NavigationMenuTrigger>Debug</NavigationMenuTrigger>
-          <NavigationMenuContent>
-            <div className="grid w-[400px] gap-3 p-4 md:w-[500px] md:grid-cols-2">
-              <NavItem title="Session Debug" href="/debug">
-                Check session and authentication state
-              </NavItem>
-              <NavItem title="GraphQL Debug" href="/debug-graphql">
-                Test GraphQL API endpoints
-              </NavItem>
-              <NavItem title="Resolvers Debug" href="/debug/resolvers">
-                Test GraphQL resolvers
-              </NavItem>
-              <NavItem title="Neo4j Debug" href="/api/debug-neo4j">
-                Test Neo4j connection
-              </NavItem>
-            </div>
-          </NavigationMenuContent>
-        </NavigationMenuItem>
+      
 
         {/* Settings */}
         <NavigationMenuItem>
@@ -134,12 +115,7 @@ export function MainNav() {
           </Link>
         </NavigationMenuItem>
 
-        {/* Products */}
-        <NavigationMenuItem>
-          <NavItem title="Products" href="/products">
-            View all products
-          </NavItem>
-        </NavigationMenuItem>
+       
       </NavigationMenuList>
     </NavigationMenu>
   );
